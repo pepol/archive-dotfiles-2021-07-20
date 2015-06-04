5b352d9abc30a1237504444b659e20695a818a58
Peter Polacik <pepol@imterra.com>
2015-06-04 19:48:45 +0200

    bootstrap: added usage information and options
    Signed-off-by: Peter Polacik <pepol@imterra.com>

---
+++ bootstrap.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+longhelp() {
+  echo "DotFiles v. 1.0"
+  echo "Copyright (c) 2015 Peter Polacik <polacik.p@gmail.com>"
+  echo
+  echo "This script bootstraps my dotfiles for use in your system."
+  echo
+  echo "Options:"
+  echo -e "\t-d  - if used, also downloads any dependencies (such as Vim plugins)"
+  echo -e "\t-h  - shows this help message"
+  echo -e "\t-u  - shows short usage information"
+  echo
+  echo "Targets:"
+  echo -e "\tall      - (default) installs all configuration files"
+  echo -e "\tgit      - installs only git files"
+  echo -e "\ti3       - installs i3 files"
+  echo -e "\tdwm      - installs dwm files"
+  echo -e "\tscripts  - installs scripts"
+  echo -e "\tbash     - installs bash files"
+  echo -e "\tzsh      - installs zsh files"
+  echo -e "\ttmux     - installs tmux files"
+  echo -e "\tutils    - installs utilities (fonts, X configs, notification configs)\n"
+  exit 1
+}
+
+usage() {
+  echo "Usage: $0 [-d] [-hu] [targets...]"
+  echo
+  exit 1
+}
+
+ALL_TARGETS=(git i3 dwm scripts bash zsh tmux utils)
+DOWNLOAD=0
+
+while getopts dhu opt; do
+  case "$opt" in
+    d) DOWNLOAD=1 ;;
+    h) longhelp ;;
+    u) usage ;;
+    \?) usage ;;
+  esac
+done
